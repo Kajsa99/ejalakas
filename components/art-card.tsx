0ad06cfcc1f2ca18eca8fdab1862ac0363bb476e
Kajsa Palmén <kajsa.palmen@gmail.com>
2026-03-26 16:48:43 +0100

    temporary img

--- components/art-card.tsx
+++ components/art-card.tsx
@@ -1,19 +1,29 @@
 import { createClient } from "@/lib/supabase/server"
 import Link from "next/link"
 import { Suspense } from "react"
-import { Card, CardContent, CardTitle } from "@/components/ui/card"
+import { Card, CardAction, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+import artImage from "../app/(photos)/image0(1).jpeg"
 
 async function ArtworkSection() {
   const supabase = await createClient()
   const { data } = await supabase.from("art").select("*").limit(1)
   return (
-    <Card>
-      <CardContent>
-        <Link href="/art">
-          {" "}
-          {data?.[0]?.name ?? "Laddar..."}
-          <CardTitle className="text-center">Till Salu</CardTitle>
-        </Link>
+    <Card className="shadow-lg">
+      <CardContent className="text-center">
+        {data?.[0]?.name ?? "Laddar..."}
+        {/* temporär img */}
+        <Image
+          src={artImage}
+          alt={data?.[0]?.name ?? "Laddar..."}
+          width={200}
+          height={800}
+        />
+        <CardAction>
+          <Link href="/art" className="text-sm text-primary">
+            Till Salu
+          </Link>
+        </CardAction>
       </CardContent>
     </Card>
   )
